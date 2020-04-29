a2b07d146b1ff2fd5a17df31374438390fd7b21a
Felix Kuehling <felix.kuehling@gmail.com>
2020-04-29 00:18:32 -0400

    Reorganize and optimize code
    Created a Keymap class with the various calc_* functions as methods.
    
    Created a TextStats class that calculates symbol frequencies and uses that
    to calculate heatmaps more efficiently. Will add bigraphs and possibly other
    useful stats later.
    
    Optimized calc_hand_runs method.
    
    Used generator expressions instead of list comprehensions in a few places.
    
    Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

--- scripts/eval_layout.py
+++ scripts/eval_layout.py
@@ -68,6 +68,19 @@ layouts = {
     "Workman": layout_WORKMAN
 }
 
+class TextStats:
+    def __calc_symbol_freqs(self):
+        self.symbol_freq = {}
+        for c in self.text:
+            if c in self.symbol_freq:
+                self.symbol_freq[c] += 1
+            else:
+                self.symbol_freq[c] = 1
+
+    def __init__(self, text):
+        self.text = text
+        self.__calc_symbol_freqs()
+
 # Translate layouts into efficient lookup tables (maps):
 # keymap_... = {symbol : (key, shift, hand, finger, x, y), ...}
 #
@@ -97,31 +110,89 @@ def calculate_key_props(key):
 
     return (hand, finger, x, y)
 
-key_props = [calculate_key_props(k) for k in range(32)]
+class Keymap:
+    key_props = [calculate_key_props(k) for k in range(32)]
+    finger_linkage = ((1   , 0.25, 0  , 0  , 0  , 0  , 0   , 0   ),
+                      (0.25, 1   , 0.5, 0  , 0  , 0  , 0   , 0   ),
+                      (0   , 0.5 , 1  , 0.1, 0  , 0  , 0   , 0   ),
+                      (0   , 0   , 0.1, 1  , 0  , 0  , 0   , 0   ),
+                      (0   , 0   , 0  , 0  , 1  , 0.1, 0   , 0   ),
+                      (0   , 0   , 0  , 0  , 0.1, 1  , 0.5 , 0   ),
+                      (0   , 0   , 0  , 0  , 0  , 0.5, 1   , 0.25),
+                      (0   , 0   , 0  , 0  , 0  , 0  , 0.25, 1   )
+    )
 
-def make_keymap(layout):
-    keymap = {}
-    for i in range(len(layout)):
-        keymap[layout[i][0]] = (i, False) + key_props[i]
-        keymap[layout[i][1]] = (i, True ) + key_props[i]
+    def __init__(self, layout):
+        self.layout = layout
+        self.keymap = {}
+        for i in range(len(layout)):
+            self.keymap[layout[i][0]] = (i, False) + self.key_props[i]
+            self.keymap[layout[i][1]] = (i, True ) + self.key_props[i]
 
-    return keymap
+    def key_strokes(self, t):
+        return sum([1 for c in t.text if c in self.keymap])
+
+    def calc_heatmap(self, t):
+        heatmap = [0 for k in range(32)]
+        for symbol, key_props in self.keymap.items():
+            if symbol in t.symbol_freq:
+                heatmap[key_props[0]] += t.symbol_freq[symbol]
+        return heatmap
+
+    def calc_finger_travel(self, t):
+        finger_pos = [(0, 0) for f in range(8)]
+        dist = [0 for f in range(8)]
+        for c in t.text:
+            if c in self.keymap:
+                (key, shift, hand, f, x, y) = self.keymap[c]
+                d = math.sqrt((x - finger_pos[f][0])**2 + (y - finger_pos[f][1])**2)
+                finger_pos[f] = (x, y)
+                dist[f] += d
+
+        return [int(a) for a in dist]
+
+    def calc_adjusted_travel(self, t):
+        finger_pos = [[0, 0] for f in range(8)]
+        dist = [0 for f in range(8)]
+        for c in t.text:
+            if c in self.keymap:
+                (key, shift, hand, f, x, y) = self.keymap[c]
+                dx = x - finger_pos[f][0]
+                dy = y - finger_pos[f][1]
+                d = math.sqrt(dx*dx + dy*dy)
+                for g in range(8):
+                    finger_pos[g][0] += self.finger_linkage[f][g]*dx
+                    finger_pos[g][1] += self.finger_linkage[f][g]*dy
+                dist[f] += d
+
+        return [int(a) for a in dist]
+
+    def calc_hand_runs(self, t, debug = 0):
+        last_hand = -1
+        run_length = 0
+        runs = ({}, {})
+        hands = [c not in self.keymap and -1 or int(self.keymap[c][2]) for c in t.text]
+        i = 0
+        for hand in hands:
+            if hand != last_hand:
+                if last_hand >= 0:
+                    if run_length in runs[last_hand]:
+                        runs[last_hand][run_length] += 1
+                    else:
+                        runs[last_hand][run_length] = 1
+                    if debug and run_length >= debug:
+                        print("Run (%d): %s" % (run_length, t.text[i-run_length:i]))
+                run_length = 0
+                last_hand = hand
+
+            run_length += 1
+            i += 1
+
+        return runs
 
 keymaps = {}
 for name, layout in layouts.items():
-    keymaps[name] = make_keymap(layout)
-
-def calculate_strokes(keymap, text):
-    return sum([1 for c in text if c in keymap])
-
-def calculate_heatmap(keymap, text):
-    heatmap = [0 for k in range(32)]
-    for c in text:
-        if c in keymap:
-            k = keymap[c][0]
-            heatmap[k] += 1
-
-    return heatmap
+    keymaps[name] = Keymap(layout)
 
 key_weights = [1,     12, 21,  6, 3,   3,  6, 21, 12,  1,
                3,  5, 12, 18, 18, 9,   9, 18, 18, 12,  5,  3,
@@ -132,86 +203,20 @@ sorted_key_weights.sort()
 def score_heatmap(heatmap):
     global key_weights, sorted_key_weigts
 
-    score = sum([a * b for a, b in zip(heatmap, key_weights)])
+    score = sum((a * b for a, b in zip(heatmap, key_weights)))
 
     sorted_heatmap = heatmap[:]
     sorted_heatmap.sort()
-    best_score = sum([a * b for a, b in zip(sorted_heatmap, sorted_key_weights)])
+    best_score = sum((a * b for a, b in zip(sorted_heatmap, sorted_key_weights)))
     sorted_heatmap.reverse()
-    worst_score = sum([a * b for a, b in zip(sorted_heatmap, sorted_key_weights)])
+    worst_score = sum((a * b for a, b in zip(sorted_heatmap, sorted_key_weights)))
 
     return (score - worst_score) / (best_score - worst_score)
 
 def normalize(heatmap, factor):
     return [h * factor for h in heatmap]
 
-def calculate_finger_travel(keymap, text):
-    finger_pos = [(0, 0) for f in range(8)]
-    dist = [0 for f in range(8)]
-    for c in text:
-        if c in keymap:
-            (key, shift, hand, f, x, y) = keymap[c]
-            d = math.sqrt((x - finger_pos[f][0])**2 + (y - finger_pos[f][1])**2)
-            finger_pos[f] = (x, y)
-            dist[f] += d
-
-    return [int(a) for a in dist]
-
-def calculate_adjusted_travel(keymap, text):
-    finger_linkage = ((1   , 0.25, 0  , 0  , 0  , 0  , 0   , 0   ),
-                      (0.25, 1   , 0.5, 0  , 0  , 0  , 0   , 0   ),
-                      (0   , 0.5 , 1  , 0.1, 0  , 0  , 0   , 0   ),
-                      (0   , 0   , 0.1, 1  , 0  , 0  , 0   , 0   ),
-                      (0   , 0   , 0  , 0  , 1  , 0.1, 0   , 0   ),
-                      (0   , 0   , 0  , 0  , 0.1, 1  , 0.5 , 0   ),
-                      (0   , 0   , 0  , 0  , 0  , 0.5, 1   , 0.25),
-                      (0   , 0   , 0  , 0  , 0  , 0  , 0.25, 1   )
-    )
-    finger_pos = [[0, 0] for f in range(8)]
-    dist = [0 for f in range(8)]
-    for c in text:
-        if c in keymap:
-            (key, shift, hand, f, x, y) = keymap[c]
-            dx = x - finger_pos[f][0]
-            dy = y - finger_pos[f][1]
-            d = math.sqrt(dx*dx + dy*dy)
-            for g in range(8):
-                finger_pos[g][0] += finger_linkage[f][g]*dx
-                finger_pos[g][1] += finger_linkage[f][g]*dy
-            dist[f] += d
-
-    return [int(a) for a in dist]
-
-def calculate_hand_runs(keymap, text, debug = 0):
-    last_hand = -1
-    run_length = 0
-    runs = ({}, {})
-    for c in text:
-        if c in keymap:
-            (key, shift, hand, f, x, y) = keymap[c]
-            hand = int(hand)
-
-            if hand == last_hand:
-                run_length += 1
-                run += c
-        else:
-            hand = -1
-
-        if hand != last_hand:
-            if last_hand >= 0:
-                if run_length in runs[last_hand]:
-                    runs[last_hand][run_length] += 1
-                else:
-                    runs[last_hand][run_length] = 1
-                if debug and run_length >= debug:
-                    print("Run (%d): %s" % (run_length, run))
-            run_length = 1
-            run = c
-            last_hand = hand
-
-    return runs
-
-def calculate_mean_runs(runs):
+def mean_runs(runs):
     mean = [0, 0]
     for hand in range(2):
         nom = 0
@@ -223,7 +228,7 @@ def calculate_mean_runs(runs):
             mean[hand] = nom / denom
     return (mean[0], mean[1])
 
-def calculate_median_runs(runs):
+def median_runs(runs):
     median = [0, 0]
     for hand in range(2):
         num = 0
@@ -239,7 +244,7 @@ def calculate_median_runs(runs):
                 break
     return (median[0], median[1])
 
-def calculate_max_runs(runs):
+def max_runs(runs):
     return (max(runs[0].keys()), max(runs[1].keys()))
 
 def mutate(layout, rand):
@@ -286,7 +291,7 @@ def anneal(layout, function):
 
     return new_layout
 
-text = sys.stdin.read()
+text = TextStats(sys.stdin.read())
 
 strokes = {}
 heatmaps = {}
@@ -299,15 +304,18 @@ norm_adj_travel = {}
 hand_runs = {}
 
 for name, keymap in keymaps.items():
-    strokes[name] = calculate_strokes(keymap, text)
-    heatmaps[name] = calculate_heatmap(keymap, text)
+    strokes[name] = keymap.key_strokes(text)
+
+    heatmaps[name] = keymap.calc_heatmap(text)
     heatmap_scores[name] = score_heatmap(heatmaps[name])
     normalized_heatmaps[name] = normalize(heatmaps[name], sum(key_weights) / strokes[name])
-    finger_travel[name] = calculate_finger_travel(keymap, text)
-    adjusted_travel[name] = calculate_adjusted_travel(keymap, text)
+
+    finger_travel[name] = keymap.calc_finger_travel(text)
+    adjusted_travel[name] = keymap.calc_adjusted_travel(text)
     normalized_travel[name] = normalize(finger_travel[name], 1000 / strokes[name])
     norm_adj_travel[name] = normalize(adjusted_travel[name], 1000 / strokes[name])
-    hand_runs[name] = calculate_hand_runs(keymap, text)
+
+    hand_runs[name] = keymap.calc_hand_runs(text)
 
 def print_heatmap(h):
     print("      %5.1f %5.1f %5.1f %5.1f %5.1f |%5.1f %5.1f %5.1f %5.1f %5.1f" % tuple(h[0:10]))
@@ -326,45 +334,45 @@ for name, score in heatmap_scores.items():
     print("Heatmap score: %f" % score)
     print("Finger travel: %d: %s" % (sum(finger_travel[name]), [int(a) for a in normalized_travel[name]]))
     print("Adjusted travel: %d: %s" % (sum(adjusted_travel[name]), [int(a) for a in norm_adj_travel[name]]))
-    print("Hand runs mean: %s" % repr(calculate_mean_runs(hand_runs[name])))
-    print("Hand runs median: %s" % repr(calculate_median_runs(hand_runs[name])))
-    print("Hand runs max: %s" % repr(calculate_max_runs(hand_runs[name])))
+    print("Hand runs mean: %s" % repr(mean_runs(hand_runs[name])))
+    print("Hand runs median: %s" % repr(median_runs(hand_runs[name])))
+    print("Hand runs max: %s" % repr(max_runs(hand_runs[name])))
 
 def optimize_runs(keymap):
     global text
 
-    runs = calculate_hand_runs(keymap, text)
-    means = calculate_mean_runs(runs)
+    runs = keymap.calc_hand_runs(text)
+    means = mean_runs(runs)
     mean = sum(means) / len(means)
     return (1.0 - 1/mean)
 
 def optimize_weights(keymap):
     global text
 
-    heatmap = calculate_heatmap(keymap, text)
+    heatmap = keymap.calc_heatmap(text)
     return score_heatmap(heatmap)
 
 def optimize(layout):
-    keymap = make_keymap(layout)
+    keymap = Keymap(layout)
     scores = (optimize_runs(keymap),
               optimize_weights(keymap))
     w = (1, 2)
-    wsum = sum([w[i] * scores[i] for i in range(len(scores))])
+    wsum = sum((w[i] * scores[i] for i in range(len(scores))))
 
     return wsum / sum(w)
 
 new_layout = anneal(layout_QWERTY, optimize)
 
-new_keymap = make_keymap(new_layout)
+new_keymap = Keymap(new_layout)
 print_layout(new_layout)
-stroke = calculate_strokes(new_keymap, text)
-heatmap = normalize(calculate_heatmap(new_keymap, text), sum(key_weights) / stroke)
-runs = calculate_hand_runs(new_keymap, text, 7)
+stroke = new_keymap.key_strokes(text)
+heatmap = normalize(new_keymap.calc_heatmap(text), sum(key_weights) / stroke)
+runs = new_keymap.calc_hand_runs(text, 7)
 print_heatmap(heatmap)
 print("Hand runs: %s" % repr(runs))
-print("Hand runs mean: %s" % repr(calculate_mean_runs(runs)))
-print("Hand runs median: %s" % repr(calculate_median_runs(runs)))
-print("Hand runs max: %s" % repr(calculate_max_runs(runs)))
+print("Hand runs mean: %s" % repr(mean_runs(runs)))
+print("Hand runs median: %s" % repr(median_runs(runs)))
+print("Hand runs max: %s" % repr(max_runs(runs)))
 
 #rand = random.Random()
 #rand.seed()
