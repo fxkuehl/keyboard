d8128e2c747b79f0f975ae5a22d9614f3c18a60b
Felix Kuehling <felix.kuehling@gmail.com>
2020-05-19 01:30:42 -0400

    Add script to pre-process text files
    Text files used for training a keyboard layout need to be proprocessed to
    remove names, typos, OCR errors and other artefacts that can distort the
    search. Use a word list file as reference and only allow words contained
    in it. Punctuations are preserved. Any missing words are output to a new
    wordlist that can be used to update the reference if needed.
    
    Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

---
+++ scripts/preproctext.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+missing_words = set()
+with open(sys.argv[1]) as wordfile:
+    words = frozenset( (s.lower() for s in wordfile.read().split()) )
+
+    in_text = sys.stdin.read()
+    in_words_sep = re.split(r"([^\w']+)", in_text)
+    
+    while in_words_sep:
+        word = in_words_sep.pop(0)
+        sep = None
+        if in_words_sep:
+            sep = in_words_sep.pop(0)
+
+        if word.lower() in words:
+            sys.stdout.write(word)
+        else:
+            missing_words.add(word.lower())
+        if sep:
+            sys.stdout.write(sep)
+
+    if len(sys.argv) >= 3:
+        with open(sys.argv[2], 'w') as missingfile:
+            for word in missing_words:
+                missingfile.write(word + '\n')
