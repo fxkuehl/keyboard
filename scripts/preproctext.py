622da414010e1c04a24917c9c3606a0e1250c6f1
Felix Kuehling <felix.kuehling@gmail.com>
2020-06-29 01:33:58 -0400

    Strip leading and trailing single apostrophes from words
    Only consider apostrophes in the middle of words as part of the word.
    
    Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

--- scripts/preproctext.py
+++ scripts/preproctext.py
@@ -11,7 +11,7 @@ with open(sys.argv[1]) as wordfile:
     in_words_sep = re.split(r"([^\w']+)", in_text)
     
     while in_words_sep:
-        word = in_words_sep.pop(0)
+        word = in_words_sep.pop(0).strip("'")
         sep = None
         if in_words_sep:
             sep = in_words_sep.pop(0)
